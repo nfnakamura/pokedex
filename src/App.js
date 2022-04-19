9fca6b75e76863c0eb33c6b3ad10340fd3b05050
nfnakamura <nfn1991@hotmail.com>
2022-04-18 21:37:09 -0300

    actualizacion con errores y loading

--- src/App.js
+++ src/App.js
@@ -1,259 +1,197 @@
-import React, { useState } from 'react';
 import axios from 'axios';
+import React, { useState } from 'react'
+import { styles } from './styles/styles';
 
 
+const App = () => {  
+  const [isLoad, setLoad] = useState(false);
+ 
 
-const styles = {
-  container: {     
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: '1px',
-    borderStyle: 'solid',
-    borderColor: 'black',
+   const [pokemon, setPokemon]= useState({
+     nombre:"",
+     hp:"",
+     attack:"",
+     defense:"",
+     specialAttack:"",
+     specialDefense:"",
+     speed:"",
 
-    background: '#fffff0',
-    padding: '5px'
-    
-  },
-  card: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: '5px',
-    borderStyle: 'double',
-    borderColor: 'black',
-    padding: '20px',
-    background: '#fffff0',
-    
-  },
+   })
 
-  cardDetails: {
-     display:'flex',
-     flexDirection:'row',
+   const [error, setError] = useState(false);
+   const [loading, setLoading] = useState(false);
 
-    }
-  ,
-  pokedex: {
-    display: 'flex',
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems:'center',
-    borderWidth: '2px',
-    borderStyle: 'solid',
-    borderColor: 'black',
-    padding: '20px',
-    background: '#ff0000',
-        
-
-  },
-  imagenPoke: {
-    display:'flex',
-    width:'100%', 
-    height:'100%',
-    marginTop:"10%"   
-  },
-
-  input: {
-    width: '95%',
-    margin: '2%',
-    padding: '2%', 
-    marginTop: '5%'         
-  },
-
-  boton: {
-    marginTop: '5%',
-    margin: '2%',
-    padding: '2%',
-    width: '100%'
-  },
-
-  imagenDesconocido:{
-    display:'flex',
-    width:'230px', 
-    padding: '5%',
-    marginTop:'2%'
-  },
-
-  spanStats:{
-    fontWeight: "bold",
-  },
-    
-  
-}
-
-const App = () => {
-
-    
-   const [isLoad, setLoad] = useState(false);
-
-
-    const [pokemon, setPokemon]= useState({
-      nombre:"",
-      hp:"",
-      attack:"",
-      defense:"",
-      specialAttack:"",
-      specialDefense:"",
-      speed:"",
-
-    })
-
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    const [nombrePokemon, setNombrePokemon] = useState({
-      nombre:""
-    })
+   const [nombrePokemon, setNombrePokemon] = useState({
+     nombre:""
+   })
 
 const handleChange = (evento) => {
-     
-      setNombrePokemon({
-          ...nombrePokemon,
-         [evento.target.name]: evento.target.value,
-      })
-
-           
-      return (evento.target.value);
     
+     setNombrePokemon({
+         ...nombrePokemon,
+        [evento.target.name]: evento.target.value,
+     })
 
-     }
+          
+     return (evento.target.value);
+   
+
+    }
+
+ const createFormData = () => {
+
+   const inputPokemon = new FormData();
+
+   inputPokemon.append("nombre", nombrePokemon.nombre);
+
+   return inputPokemon;
+  }
 
 
+  const obternerDatos = async () => {
+   try {
 
-  const createFormData = () => {
-
-    const inputPokemon = new FormData();
-
-    inputPokemon.append("nombre", nombrePokemon.nombre);
-
-    return inputPokemon;
+    setLoading(true);
+     const pokemon = await axios(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon.nombre.toLowerCase()}`)
+     
+     setPokemon(pokemon.data);       
+     setError(false);     
+     setLoad(true);
+     setLoading(false)
+      
+          
+     return (pokemon.data)
+     
+   } catch (error) {
+     
+     setError(true);
+     setLoading(false);    
+     
+     return error;
+     
    }
 
 
-   const obternerDatos = async () => {
-    try {
 
-      const pokemon = await axios(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon.nombre.toLowerCase()}`)
-      
-      
-      setPokemon(pokemon.data);
+};  
+ 
+
+  const handleSubmit = (evento) => {    
+
+   evento.preventDefault(); 
+  
+   const inputPokemon = createFormData();
+ 
+
+   if(inputPokemon.get('nombre')!==""){
+
+     obternerDatos(); 
         
-      setError(false);
-      setLoading(false);
-      setLoad(true);
-       
-           
-      return (pokemon.data)
-      
-    } catch (error) {
-      
+
+     
+   }else{
+
       setError(true);
-      setLoading(false);    
+   }
+
       
-      return error;
       
-    }
+ }   
 
 
+   function nombrepokemonCapital (){  
 
- };  
-  
+    const nombre = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
 
-   const handleSubmit = (evento) => {    
 
-    evento.preventDefault(); 
+    return nombre;
    
-    const inputPokemon = createFormData();
-  
+ };
 
-    if(inputPokemon.get('nombre')!==""){
+ return (
+   <>
+   
+    <div style={styles.container}>
 
-      obternerDatos();      
+      <h1>Pokedex</h1>
 
+       <div style={styles.pokedex}>
       
-    }else{
-
-       setError(true);
-    }
-
-       
-       
-  }   
-
-
-    function nombrepokemonCapital (){  
-
-     const nombre = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-
-
-     return nombre;
-    
-  };
-
-  return (
-    <>
-    
-     <div style={styles.container}>
-
-     <h1>Pokedex</h1>
-
-        <div style={styles.pokedex}>
         <div>
-          {isLoad && !error ?
+         {isLoad && !error && !loading ? 
+           <div style={styles.card}>
+
+             <h2> {nombrepokemonCapital()}</h2>
+             <div style={styles.cardDetails}>
+               <img style={styles.imagenPoke} alt='Imagen Pokemon' src={pokemon.sprites.front_default}></img>
+               <div>
+                 <p><span style={styles.spanStats}>Hp:</span> {pokemon.stats[0].base_stat}</p>
+                 <p><span style={styles.spanStats}>Attack:</span> {pokemon.stats[1].base_stat}</p>
+                 <p><span style={styles.spanStats}>Defense:</span> {pokemon.stats[2].base_stat} </p>
+                 <p><span style={styles.spanStats}>Special Attack:</span> {pokemon.stats[3].base_stat} </p>
+                 <p><span style={styles.spanStats}>Special Defense:</span> {pokemon.stats[4].base_stat} </p>
+                 <p><span style={styles.spanStats}>Speed:</span> {pokemon.stats[5].base_stat} </p>
+               </div>              
+             </div>              
+          </div>            
+          : error && !loading ?
+             <div style={styles.card}>
+               <h2>¡Ups, pokemon no encontrado!</h2>
+               <div style={styles.cardDetails}>
+                 <img src='https://images.hive.blog/0x0/https://cdn.steemitimages.com/DQmbHKnboQUxh3m9mT6Yy2oQfSww9VEyR6ZPRoW69Ps2KDZ/012_question_mark.png' alt='Pokemon no encontrado' style={styles.imagenDesconocido}></img>                   
+               </div>     
+             </div>  
+         : !loading && !error & !isLoad ?
+
             <div style={styles.card}>
-
-              <h2> {nombrepokemonCapital()}</h2>
-              <div style={styles.cardDetails}>
-                <img style={styles.imagenPoke} alt='Imagen Pokemon' src={pokemon.sprites.front_default}></img>
-                <div>
-                  <p><span style={styles.spanStats}>Hp:</span> {pokemon.stats[0].base_stat}</p>
-                  <p><span style={styles.spanStats}>Attack:</span> {pokemon.stats[1].base_stat}</p>
-                  <p><span style={styles.spanStats}>Defense:</span> {pokemon.stats[2].base_stat} </p>
-                  <p><span style={styles.spanStats}>Special Attack:</span> {pokemon.stats[3].base_stat} </p>
-                  <p><span style={styles.spanStats}>Special Defense:</span> {pokemon.stats[4].base_stat} </p>
-                  <p><span style={styles.spanStats}>Speed:</span> {pokemon.stats[5].base_stat} </p>
-                </div>              
-              </div>              
-           </div>            
-           : 
-              <div style={styles.card}>
-                <h2> Pokemon no encontrado</h2>
-                <div style={styles.cardDetails}>
-                  <img src='https://images.hive.blog/0x0/https://cdn.steemitimages.com/DQmbHKnboQUxh3m9mT6Yy2oQfSww9VEyR6ZPRoW69Ps2KDZ/012_question_mark.png' alt='Pokemon no encontrado' style={styles.imagenDesconocido}></img>                   
-                </div>     
-              </div>           
-          }
-         
-        </div>
-
-          <div>
-            <input style={styles.input}
-             type='text'
-             placeholder='Buscá tu pokemon'
-             name='nombre'
-             onChange={handleChange}
-             value={nombrePokemon.nombre}
+               <h2> Buscador</h2>
+               <div style={styles.cardImagenes}>
+                 <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png' alt='PokemonLogo' style={styles.imagenBuscador}></img>                   
+                 <img src='https://cdn-icons-png.flaticon.com/512/2052/2052715.png' alt='Lupa' style={styles.imagenLupa}></img>                   
              
-            >
-            </input> 
+               </div>     
+             </div>  
+                               
+         : loading && 
+         <div style={styles.card}>
+                <div style={styles.cardDetails}>
+                  <img src='https://c.tenor.com/e6J4X97EZkIAAAAi/ash-now.gif' alt='Loading...' style={styles.imagenLoading}></img>                   
+                  
+                </div>  
+                
+                   
+         </div>
+         
+         
+         }
         
-            <input style={styles.boton}
-             type='submit'
-             value='Buscar'
-             onClick={handleSubmit}
-            >
-            </input>
-          </div>
+       </div>
 
-        </div>
-        
-     </div>
+         <div>
+           <input style={styles.input}
+            type='text'
+            placeholder='Buscá tu pokemon'
+            name='nombre'
+            onChange={handleChange}
+            value={nombrePokemon.nombre}
             
-    </>
-  )
+           >
+           </input> 
+       
+           <input style={styles.boton}
+            type='submit'
+            value='Buscar'
+            onClick={handleSubmit}
+           >
+           </input>
+         </div>
+
+       </div>
+       
+    </div>
+           
+   </>
+ )
 }
 
 export default App
---
+++ src/styles/styles.jsx
@@ -0,0 +1,115 @@
+export const styles = {
+    container: {     
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: '1px',
+      borderStyle: 'solid',
+      borderColor: 'black',  
+      background: '#fffff0',
+      padding: '5px',
+      
+      
+    },
+    card: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: '5px',
+      borderStyle: 'double',
+      borderColor: 'black',
+      background: '#fffff0',
+      width: '100%',
+      
+         
+    },
+  
+    cardDetails: {
+       display:'flex',
+       flexDirection:'row',
+       marginRight:'10%',  
+      }
+    ,
+    pokedex: {
+      display: 'flex',
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      borderColor: 'black',
+      padding: '20px',
+      background: '#ff0000',
+      width:'400px',
+      height:'450px',
+          
+  
+    },
+    imagenPoke: {
+      display:'flex',
+      width:'230px', 
+      height:'100%',
+         
+    },
+  
+    input: {
+      width: '95%',
+      margin: '2%',
+      padding: '2%', 
+      marginTop: '5%'         
+    },
+  
+    boton: {
+      marginTop: '5%',
+      margin: '2%',
+      padding: '2%',
+      width: '100%'
+    },
+  
+    imagenDesconocido:{
+      display:'flex',
+      width:'230px', 
+      padding: '5%',
+      marginTop:'2%'
+    },
+  
+    spanStats:{
+      fontWeight: "bold",
+    },
+
+    
+      imagenLoading:{
+         display: "flex",
+         justifyContent: "center",
+         alignItems: "center",         
+      },
+      
+      imagenBuscador:{
+        display:'flex',
+        width:'250px',         
+        marginBottom: '5%'
+       
+
+      },
+
+      imagenLupa: {
+        display:'flex',
+        width:'150px',         
+        
+      },
+
+      cardImagenes:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems: 'center',
+        padding: '5%',
+        
+        
+
+      }
+    
+  }
+  
